cbab6e7484f0db7f9e9e2ad28f9d9c7c892b815e
Venus <socratep@gmail.com>
2014-06-04 14:45:41 -0400

    Add AlarmInterface folder

---
+++ AlarmInterface/Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms647');
+    define('DB_USER', 'simplecmsuser872');
+    define('DB_PASSWORD', 'CGJD%6]Rm2!z');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', '123456');
+?>
